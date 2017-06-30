557c33e9e6e7132f4b27f5c3f7a599658784e453
Kealan Thompson <kealanthompson@hotmail.com>
2017-06-30 17:45:05 +0100

    Update isVisible.java

--- isVisible.java
+++ isVisible.java
@@ -5,9 +5,8 @@ import org.openqa.selenium.WebElement;
 
 public class isVisible {
 
-    public SELF isVisible(SELF self) {
-            assert this.isDisplayed() : true;
+    public WebElement isVisible(WebElement element) {
+            assert element.isDisplayed() : true;
+            return element;
     }
 }
-//just focused on mechanism of identifing whether the condition is satisfied or not
-//still unsure whether basing on self type and returning the object is the best decision
