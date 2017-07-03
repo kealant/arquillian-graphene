60e2b67baee01f7080a3cc5fb5f328ac376f9369
Kealan Thompson <kealanthompson@hotmail.com>
2017-07-03 17:00:39 +0100

    Update exists.java

--- exists.java
+++ exists.java
@@ -1,17 +1,18 @@
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
-class Exists {
-    public SELF Exists(SELF self){
-        try{
-        assert findElement(this) != null;
-        return self;
-        }
-        catch (NoSuchElementException nosuchelenentexception){
-            return false; //throw an exception?
-        }
-    }
+class Exists{
+   public WebElement Exists(String element, String locator, WebDriver driver){
+       String [] posLocators = {"xpath", "className", "cssSelector", "id", "linkText", "name", "partialLinkText", "tagName"};
+       if (!Arrays.asList(posLocators).contains(locator)){
+           System.out.println("Invalid locator string, must supply one of the By locator options");
+       }
+       try{
+            WebElement elem = driver.findElement(By.locator(element));
+        return elem;
+       }
+
+   }
 }
-
-// unsure about this way of writing assertions
-
-// .getWebElement() instead?
