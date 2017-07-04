62b6307f008d961fec13374316afa73ddcb9fffa
Kealan Thompson <kealanthompson@hotmail.com>
2017-07-04 14:50:55 +0100

    Update disabled.java

--- disabled.java
+++ disabled.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebElement;
 
 public class disabled {
     public WebElement disabled(WebElement element){
-        assert element.isEnabled() : false;
+        assert element.isEnabled() == false : "Element is actually enabled";
         return element;
     }
 }
