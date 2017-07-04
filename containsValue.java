c5875dc4313916ea6e235873f9a9c38b0d19b473
Kealan Thompson <kealanthompson@hotmail.com>
2017-07-04 14:54:58 +0100

    Update containsValue.java

--- containsValue.java
+++ containsValue.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebElement;
 
 public class containsValue{
     public WebElement containsValue(WebElement element, String expected){
-        assert element.getText().equals(expected);
+        assert element.getText().equals(expected) : "The value contained in this element does not match the expected string." ;
         return element;
     }
 }
