4e93ad7f1eb8d6874b39cec0e4d999e3f8cd762a
Kealan Thompson <kealanthompson@hotmail.com>
2017-07-05 16:18:31 +0100

    Update and rename clearInput.java to ClearInput.java

--- ClearInput.java
+++ ClearInput.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.WebElement;
 
-public class clearInput {
+public class ClearInput {
     public WebElement cleared(WebElement element){
         assert element.getText().equals("") == true : "The input area is not clear." ;
         return element;
