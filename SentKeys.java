9294066c782af512bc756b327129167881e02b3c
Kealan Thompson <kealanthompson@hotmail.com>
2017-07-05 16:21:34 +0100

    Update and rename sentKeys.java to SentKeys.java

--- SentKeys.java
+++ SentKeys.java
@@ -1,7 +1,7 @@
 
 import org.openqa.selenium.WebElement;
 
-public class sentKeys {
+public class SentKeys {
     WebElement sentKeys(WebElement element, String expected){
         assert element.getText.equals(expected);
         return element;
