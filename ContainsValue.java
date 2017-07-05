c14cdaa5c2657657d53f45bfccc92029f96b64ee
Kealan Thompson <kealanthompson@hotmail.com>
2017-07-05 16:19:17 +0100

    Update and rename containsValue.java to ContainsValue.java

--- ContainsValue.java
+++ ContainsValue.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.WebElement;
 
-public class containsValue{
+public class ContainsValue{
     public WebElement containsValue(WebElement element, String expected){
         assert element.getText().equals(expected) : "The value contained in this element does not match the expected string." ;
         return element;
